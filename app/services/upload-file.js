2036db4fd7a15e4335e24062c7007e68885798fd
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-09-08 10:13:50 -0700

    added S3 service

---
+++ app/services/upload-file.js
@@ -0,0 +1,34 @@
+//dependencies
+const express = require('express');
+
+//dependencies to upload to S3
+var aws = require('aws-sdk')
+var multer = require('multer')
+var multerS3 = require('multer-s3')
+
+//AWS configuration
+aws.config.update({
+    secretAccessKey: '...',
+    accessKeyId: '...',
+    region: 'us-west-1'
+})
+
+//new S3 instance
+var s3 = new aws.S3()
+ 
+//configure upload
+var upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'middle-uci-bucket',
+    acl: 'public-read',
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: 'TESTING_META_DATA'});
+    },
+    key: function (req, file, cb) {
+      cb(null, 'Test')
+    }
+  })
+})
+ 
+module.exports = upload
