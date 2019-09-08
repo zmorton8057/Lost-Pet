fd88ec27e97cb0c43342a3996e78075d601f6a5a
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-09-08 10:09:34 -0700

    added service folder

--- app/routes/api.routes.js
+++ app/routes/api.routes.js
@@ -2,30 +2,34 @@ var express = require('express')
 var router = express.Router()
 var User = require('../resources/users')
 
-router.post('/api/users', function (req, res) {
-    User.create(req.body)
-    .then(function(resp) {
-        res.send('created')
-    })
-    .catch(function (err) {
-        throw err
-    })
-})
+// router.post('/api/users', function (req, res) {
+//     User.create(req.body)
+//     .then(function(resp) {
+//         res.send('created')
+//     })
+//     .catch(function (err) {
+//         throw err
+//     })
+// })
 
-router.get('/api/users', function (req, res) {
-    res.send('get all')
-})
+// router.get('/api/users', function (req, res) {
+//     res.send('get all')
+// })
 
-router.get('/api/users/:id', function (req, res) {
-    res.send('get one')
-})
+// router.get('/api/users/:id', function (req, res) {
+//     res.send('get one')
+// })
 
-router.put('/api/users/:id', function (req, res) {
-    res.send('update')
-})
+// router.put('/api/users/:id', function (req, res) {
+//     res.send('update')
+// })
 
-router.delete('/api/users/:id', function (req, res) {
-    res.send('delete')
-})
+// router.delete('/api/users/:id', function (req, res) {
+//     res.send('delete')
+// })
 
+router.get('/:owner/:pet', function(req, res) {
+    var ownerName = req.params.owner;
+    var petName = req.params.pet;
+})
 module.exports = router
