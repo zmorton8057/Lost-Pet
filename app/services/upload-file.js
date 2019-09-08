1ea9d7bf5360029b99f5ecc25f1f3bf9733c0327
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-09-08 11:39:04 -0700

    added access keys

--- app/services/upload-file.js
+++ app/services/upload-file.js
@@ -6,10 +6,13 @@ var aws = require('aws-sdk')
 var multer = require('multer')
 var multerS3 = require('multer-s3')
 
+//config keys
+var keys = require('../../config/config');
+
 //AWS configuration
 aws.config.update({
-    secretAccessKey: '...',
-    accessKeyId: '...',
+    secretAccessKey: keys.AWSSecretKey,
+    accessKeyId: keys.AWSAccessKey,
     region: 'us-west-1'
 })
 
