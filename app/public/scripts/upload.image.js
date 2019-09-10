ef81ad3146372e7283a57558c14810f79e9b483c
Vincent Nguyen <vpvnguyen@gmail.com>
2019-09-10 00:43:24 -0700

    added ability to remove images on click

--- app/public/scripts/upload.image.js
+++ app/public/scripts/upload.image.js
@@ -8,21 +8,25 @@ $(document).ready(function () {
         $(`input[id='upload-image']`).trigger('click');
         document.getElementById('upload-image').addEventListener('change', readFile);
     });
+
+    // remove image on click
+    $(document).on('click', '.remove-img', function () {
+        var thisImg = $(this).attr('src');
+        $(this).remove();
+        images = images.filter(a => a !== thisImg)
+    });
 });
 
 // read uploaded image
 function readFile() {
     if (this.files && this.files[0]) {
-        var FR = new FileReader();
 
+        var FR = new FileReader();
         FR.addEventListener('load', function (base64Img) {
 
             // pass base64 image to be uploaded to jumbotron
             displayPhoto(base64Img.target.result);
-
-
         });
-
         FR.readAsDataURL(this.files[0]);
     }
 };
@@ -37,14 +41,16 @@ function retakePhoto(image) {
 
 function displayPhoto(image) {
     $('.camera').empty();
-    $('#jumbotron-top').append(`<img src="${image}" alt="image" class="uploaded-images responsive text-center mb-2">`);
+    $('#jumbotron-top').append(`<img src="${image}" alt="image" class="uploaded-images responsive text-center mb-2 remove-img">`);
     retakePhoto(image);
-
 };
+
 function maxPhotos(image) {
     images.push(image);
-    if (images.length === 3) {
+    if (images.length > 3) {
+        retakePhoto(image);
+    } else if (images.length === 3) {
         $('#form-message').empty();
         $('#form-message').text('Max photos allowed!');
     }
-}
+};
