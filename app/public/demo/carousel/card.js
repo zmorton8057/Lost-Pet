4033e17a4cbd4f780c4ef9f0cb4ca215fb67a59d
Vincent Nguyen <vpvnguyen@gmail.com>
2019-09-09 23:44:01 -0700

    fixed out of scope get bug

--- app/public/demo/carousel/card.js
+++ app/public/demo/carousel/card.js
@@ -40,14 +40,15 @@ $('html body').keydown(function (e) {
     }
 });
 
-$.get('/query/petinfo/allpets')
-        .then(function(response){
-            
-            var data = response;
-            console.log(response)
-            for (var i=0;i<data.length;i++){
-                $( ".profile" ).append( '<div class="card"><img class="img-fluid image-profile" src="puppy-dog.jpg" alt="John" style="width:100%"><h1>John Doe</h1><p class="title">CEO & Founder, Example</p><p>Harvard University</p><a href="#"> <i class="fa fa-dribbble"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"> <i class="fa fa-linkedin"></i></a><a href="#"> <i class="fa fa-facebook"></i></a><p><button>Contact</button></p></div>' );
-            }
-        
-        }) 
-// $( ".profile" ).append( '<div class="card"><img class="img-fluid image-profile" src="puppy-dog.jpg" alt="John" style="width:100%"><h1>John Doe</h1><p class="title">CEO & Founder, Example</p><p>Harvard University</p><a href="#"> <i class="fa fa-dribbble"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"> <i class="fa fa-linkedin"></i></a><a href="#"> <i class="fa fa-facebook"></i></a><p><button>Contact</button></p></div>' );
+// // this function is causing global get of allpets. this need to be wrapped in a function.
+// $.get('/allpets')
+//     .then(function (response) {
+
+//         var data = response;
+//         console.log(response)
+//         for (var i = 0; i < data.length; i++) {
+//             $(".profile").append('<div class="card"><img class="img-fluid image-profile" src="puppy-dog.jpg" alt="John" style="width:100%"><h1>John Doe</h1><p class="title">CEO & Founder, Example</p><p>Harvard University</p><a href="#"> <i class="fa fa-dribbble"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"> <i class="fa fa-linkedin"></i></a><a href="#"> <i class="fa fa-facebook"></i></a><p><button>Contact</button></p></div>');
+//         }
+
+//     })
+// // $( ".profile" ).append( '<div class="card"><img class="img-fluid image-profile" src="puppy-dog.jpg" alt="John" style="width:100%"><h1>John Doe</h1><p class="title">CEO & Founder, Example</p><p>Harvard University</p><a href="#"> <i class="fa fa-dribbble"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"> <i class="fa fa-linkedin"></i></a><a href="#"> <i class="fa fa-facebook"></i></a><p><button>Contact</button></p></div>' );
