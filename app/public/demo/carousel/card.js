3575c4f74c2b1996d84157227da0920d1ebf6e30
Zac Morton <zmorton8057@gmail.com>
2019-09-10 08:51:34 -0700

    Added 5 cards to the home page, need Data from database to complete properly as well as image src

--- app/public/demo/carousel/card.js
+++ app/public/demo/carousel/card.js
@@ -1,3 +1,5 @@
+var array = [];
+
 $num = $('.my-card').length;
 $even = $num / 2;
 $odd = ($num + 1) / 2;
@@ -40,14 +42,38 @@ $('html body').keydown(function (e) {
     }
 });
 
-$.get('/query/petinfo/allpets')
+$.get('/query/petinfo/allpets', function(req, res,){
+    
+
+
+})
         .then(function(response){
-            
+            // stringify the object returned from response
             var data = response;
-            console.log(response)
-            for (var i=0;i<data.length;i++){
-                $( ".profile" ).append( '<div class="card"><img class="img-fluid image-profile" src="puppy-dog.jpg" alt="John" style="width:100%"><h1>John Doe</h1><p class="title">CEO & Founder, Example</p><p>Harvard University</p><a href="#"> <i class="fa fa-dribbble"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"> <i class="fa fa-linkedin"></i></a><a href="#"> <i class="fa fa-facebook"></i></a><p><button>Contact</button></p></div>' );
+            JSON.stringify(data)
+            console.log(data[0])
+            var lost = data[0].lost_status
+            console.log(data[0].lost_status)
+            function lostStatus(){
+            if (lost === true){
+                return lost ="MISSING" 
+            }    else {
+                return lost ="SAFE"
             }
+            lostStatus()
+        }
+            ////appending each profile to the dom
+            $( ".profile1" ).append( `<div class="card"><img class="img-fluid image-profile" src="${data[0].pet_image1}" alt="${data[0].pet_name} the ${data[0].pet_type}" style="width:100%"><h1>${data[0].lost_status}</h1><h4>${data[0].pet_name} the ${data[0].pet_type}</h4><p class="title">${data[0].sex} - Age: ${data[0].pet_age} yo </p><p class="title">${data[0].pet_breed}</p><p class="title">Location: ${data[0].pet_zip}</p><h5>Coat Description:</h5><p class="title">Color: ${data[0].color}</p><p class="title">Coat Length: ${data[0].coat_length}</p><p class="title">Coat Type: ${data[0].coat_type}</p><p><button>Contact</button></p></div>` );
+
+            $( ".profile2" ).append( `<div class="card"><img class="img-fluid image-profile" src="${data[1].pet_image1}" alt="${data[1].pet_name} the ${data[1].pet_type}" style="width:100%"><h1>${data[1].lost_status}</h1><h4>${data[1].pet_name} the ${data[1].pet_type}</h4><p class="title">${data[1].sex} - Age: ${data[1].pet_age} yo </p><p class="title">${data[1].pet_breed}</p><p class="title">Location: ${data[1].pet_zip}</p><h5>Coat Description:</h5><p class="title">Color: ${data[1].color}</p><p class="title">Coat Length: ${data[1].coat_length}</p><p class="title">Coat Type: ${data[1].coat_type}</p><p><button>Contact</button></p></div>` );
+        
+            $( ".profile3" ).append( `<div class="card"><img class="img-fluid image-profile" src="${data[2].pet_image1}" alt="${data[2].pet_name} the ${data[2].pet_type}" style="width:100%"><h1>${data[2].lost_status}</h1><h4>${data[2].pet_name} the ${data[2].pet_type}</h4><p class="title">${data[2].sex} - Age: ${data[2].pet_age} yo </p><p class="title">${data[2].pet_breed}</p><p class="title">Location: ${data[2].pet_zip}</p><h5>Coat Description:</h5><p class="title">Color: ${data[2].color}</p><p class="title">Coat Length: ${data[2].coat_length}</p><p class="title">Coat Type: ${data[2].coat_type}</p><p><button>Contact</button></p></div>` );
+        
+            $( ".profile4" ).append( `<div class="card"><img class="img-fluid image-profile" src="${data[3].pet_image1}" alt="${data[3].pet_name} the ${data[3].pet_type}" style="width:100%"><h1>${data[3].lost_status}</h1><h4>${data[3].pet_name} the ${data[3].pet_type}</h4><p class="title">${data[3].sex} - Age: ${data[3].pet_age} yo </p><p class="title">${data[3].pet_breed}</p><p class="title">Location: ${data[3].pet_zip}</p><h5>Coat Description:</h5><p class="title">Color: ${data[3].color}</p><p class="title">Coat Length: ${data[3].coat_length}</p><p class="title">Coat Type: ${data[3].coat_type}</p><p><button>Contact</button></p></div>` );
+
+            $( ".profile5" ).append( `<div class="card"><img class="img-fluid image-profile" src="${data[4].pet_image1}" alt="${data[4].pet_name} the ${data[4].pet_type}" style="width:100%"><h1>${data[4].lost_status}</h1><h4>${data[4].pet_name} the ${data[4].pet_type}</h4><p class="title">${data[4].sex} - Age: ${data[4].pet_age} yo </p><p class="title">${data[4].pet_breed}</p><p class="title">Location: ${data[4].pet_zip}</p><h5>Coat Description:</h5><p class="title">Color: ${data[4].color}</p><p class="title">Coat Length: ${data[4].coat_length}</p><p class="title">Coat Type: ${data[4].coat_type}</p><p><button>Contact</button></p></div>` );
         
         }) 
-// $( ".profile" ).append( '<div class="card"><img class="img-fluid image-profile" src="puppy-dog.jpg" alt="John" style="width:100%"><h1>John Doe</h1><p class="title">CEO & Founder, Example</p><p>Harvard University</p><a href="#"> <i class="fa fa-dribbble"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"> <i class="fa fa-linkedin"></i></a><a href="#"> <i class="fa fa-facebook"></i></a><p><button>Contact</button></p></div>' );
+
+        
+       
